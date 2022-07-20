d170370862ce5163f93467b3799425263330b328
ibadet ismayilov <ibadetismayilov20@gmail.com>
2022-07-21 02:29:06 +0400

    navbar's done

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import './styles/responsive.css'
+import { Routes, Route } from'react-router-dom'
+import { Home } from './pages/Home';
+import { Navbar } from './components/Navbar';
+import { About } from './pages/About';
+import { Shop } from './pages/Shop';
+import { Contact } from './pages/Contact';
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/MobileNavbar.css'
+interface Props {
+    isMobileNavOpen: boolean
+    setIsMobileNavOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+export const MobileNavbar = ({isMobileNavOpen, setIsMobileNavOpen}: Props) => {
+  return (
+    <div className='mobile-navbar'>
+        <div className='mobile-navbar__flex'>
+
+        </div>
+    </div>
+  )
+}
